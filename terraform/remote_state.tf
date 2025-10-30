71973fbd345a054d71abcd0920146c95270c29a7
alkalax <alkalax@github.com>
2025-10-30 14:13:22 +0100

    Use different state file

--- terraform/remote_state.tf
+++ terraform/remote_state.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "tf-state-rg"
     storage_account_name = "tfstatesctrial"
     container_name       = "tfstate"
-    key                  = ".tfstate"
+    key                  = "terraform.tfstate"
     use_azuread_auth     = true
     use_oidc             = true
   }
