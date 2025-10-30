05190acde68c3d77ddef951994c224c530e844e6
alpineFrux <57093460+paraskeuos@users.noreply.github.com>
2025-10-30 12:06:15 +0100

    Feature/initial stack (#1)
    * Add ansible
    
    * Add terraform
    
    * Add workflows
    
    ---------
    
    Co-authored-by: alkalax <alkalax@github.com>

---
+++ terraform/remote_state.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "tf-state-rg"
+    storage_account_name = "tfstatesctrial"
+    container_name       = "tfstate"
+    key                  = ".tfstate"
+    use_azuread_auth     = false
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "resource_group_location" {
+  type        = string
+  default     = "westus"
+  description = "Location of the resource group."
+}
+
+variable "resource_group_name" {
+  type        = string
+  default     = "westusrg"
+}
+
+variable "stack_name" {
+  type        = string
+  default     = "keycloak-podman"
+}
